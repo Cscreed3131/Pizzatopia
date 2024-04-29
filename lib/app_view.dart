99133e8417da1d74b33bd6522c0130729e309239
Cscreed3131 <anubhav82355@gmail.com>
2024-04-29 11:48:51 +0530

    added auth screens and bloc logic and more

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizzatopia/blocs/authentication_bloc/authentication_bloc.dart';
+
+import 'screens/auth/views/welcome_screen.dart';
+import 'screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pizzatopia',
+      theme: ThemeData(
+          colorScheme: ColorScheme.light(
+        background: Colors.grey.shade100,
+        onBackground: Colors.black,
+        primary: Colors.blue,
+        onPrimary: Colors.white,
+      )),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: (context, state) {
+        if (state.status == AuthenticationStatus.authenticated) {
+          return const HomeScreen();
+        } else {
+          return const WelcomeScreen();
+        }
+      }),
     );
   }
 }
